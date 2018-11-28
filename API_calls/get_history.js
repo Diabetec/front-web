5d8c2e622328c0640d0f5460f2f7807a4737a2cc
Margot Duek <margotduek@gmail.com>
2018-11-28 15:17:56 -0600

    comidas

--- API_calls/get_history.js
+++ API_calls/get_history.js
@@ -20,18 +20,19 @@ $( document ).ready(function() {
 		    xhr.setRequestHeader('Authorization', 'Bearer ' + user_token);
 			},
 			success: function(result){
+				console.log("before");
 				$('#getResultDiv ul').empty();
-				// var custList = "";
-				// $.each(result, function(i, customer){
-				// 	$('#getResultDiv .list-group').append(customer.firstname + " " + customer.lastname + "<br>")
-				// });
-				console.log(result.name);
-				document.getElementById('user_name').innerHTML = result.name;
-				document.getElementById('user_email').innerHTML = result.email;
-				document.getElementById('user_email2').innerHTML = result.email;
-				document.getElementById('user_weight').innerHTML = result.weight;
-				document.getElementById('user_height').innerHTML = result.heigh;
-				document.getElementById('user_age').innerHTML = result.age;
+				var custList = "";
+				$.each(result, function(i, customer.foods){
+					$('#getResultDiv .list-group').append(customer.foods.label + "<br> Calorías: " + customer.foods.calories "<br> Carboidratos: " + customer.foods.carbs )
+				});
+				// console.log("food" + result.foods);
+				// document.getElementById('user_name').innerHTML = result.name;
+				// document.getElementById('user_email').innerHTML = result.email;
+				// document.getElementById('user_email2').innerHTML = result.email;
+				// document.getElementById('user_weight').innerHTML = result.weight;
+				// document.getElementById('user_height').innerHTML = result.heigh;
+				// document.getElementById('user_age').innerHTML = result.age;
 
 				console.log("Success: ", result);
 			},
